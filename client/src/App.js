83f2f5413ed5c2728293472e942c158392628b70
Amanda Klenk <amandamklenk3@gmail.com>
2022-08-31 13:18:16 -0400

    set up App.js to instantiate a new Apollo server

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,48 @@
 import React from 'react';
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { setContext } from '@apollo/client/link/context';
+import Navbar from './components/Navbar';
+
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
-import Navbar from './components/Navbar';
+
+// establish new link to GraphQL server
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+// retrieves token from localStorage and sets headers to include token
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+// instantiate Apollo Client and create connection to the API endpoint and new cache
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <Router>
-      <>
-        <Navbar />
-          <Routes>
-            <Route 
-              path='/' 
-              element={<SearchBooks />} 
-            />
-            <Route 
-              path='/saved' 
-              element={<SavedBooks />} 
-            />
-            <Route 
-              path='*'
-              element={<h1 className='display-2'>Wrong page!</h1>} 
-            />
-          </Routes>
-      </>
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <>
+          <Navbar />
+            <Routes>
+              <Route path='/' element={<SearchBooks />} />
+              <Route path='/saved' element={<SavedBooks />} />
+              <Route path='*' element={<h1 className='display-2'>Wrong page!</h1>} />
+            </Routes>
+        </>
+      </Router>
+    </ApolloProvider>
   );
 }
 
