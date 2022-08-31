cc20e43f516496d38b71698ec395322af114df04
Amanda Klenk <amandamklenk3@gmail.com>
2022-08-31 10:44:19 -0400

    set up mutations in resolvers.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -15,8 +15,59 @@ const resolvers = {
             }
 
             throw new AuthenticationError('Not logged in');
-        }
-    }
-}
+        },
 
-module.exports = resolvers; 
+        Mutation: {
+            addUser: async (parent, args) => {
+                const user = await User.create(args);
+                const token = signToken(user);
+    
+                return { token, user };
+            },
+            login: async (parent, { email, password }) => {
+                const user = await User.findOne({ email });
+    
+                if (!user) {
+                    throw new AuthenticationError('Incorrect credentials');
+                }
+    
+            const correctPw = await user.isCorrectPassword(password);
+    
+            if(!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+    
+            const token = signToken(user);
+            return { token, user };
+            },
+            saveBook: async (parent, args, context) => {
+                if (context.user) {
+                  await User.findByIdAndUpdate(
+                    { _id: context.user._id },
+                    { $addToSet: { savedBooks: book.BookInput } },
+                    { new: true }
+                  );
+              
+                  return book;
+                }
+              
+                throw new AuthenticationError('You need to be logged in!');
+              },
+              removeBook: async (parent, args) => {
+                    if (context.user) {
+                      await User.findByIdAndUpdate(
+                        { _id: context.user._id },
+                        { $pull: { savedBooks: book.bookId } },
+                        { new: true }
+                      );
+                  
+                      return user;
+                    }
+                  
+                    throw new AuthenticationError('You need to be logged in!');
+              }
+        },
+    }
+};
+
+module.exports = resolvers;
