d62d2d2052d7f4fea5b044cc185eb9be3d85cc1a
Amanda Klenk <amandamklenk3@gmail.com>
2022-09-01 09:50:48 -0400

    set up logic for handleFormSubmit for LoginForm.js

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 import { useMutation } from '@apollo/client';
 import { ADD_USER } from '../utils/mutations';
-// import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
@@ -39,6 +38,7 @@ const SignupForm = () => {
     Auth.login(data.addUser.token);
   } catch (e) {
     console.error(e);
+    setShowAlert(true);
   }
     setUserFormData({
       username: '',
